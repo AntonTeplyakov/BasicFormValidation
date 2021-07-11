5b77ad4613cae400c8d941c9e5baf90942a0cf99
Anton Teplyakov <86085779+AntonTeplyakov@users.noreply.github.com>
2021-07-11 16:52:42 +1000

    working form
    working sample js validation form

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const form = document.querySelector('.signup-form');
 const feedback = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
 form.addEventListener('submit',e => {
     e.preventDefault();
 
     const username = form.username.value;
-    const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
     if (usernamePattern.test(username)){
         feedback.textContent = 'that username is valid!';
@@ -15,5 +15,10 @@ form.addEventListener('submit',e => {
 })
 
 form.username.addEventListener('keyup', e =>{
-    console.log(e.target.value, form.username.value);
+    console.log(e);
+    if (usernamePattern.test(e.target.value)){
+        form.username.setAttribute('class','success');
+    }else{
+        form.username.setAttribute('class','error'); 
+    }   
 });
